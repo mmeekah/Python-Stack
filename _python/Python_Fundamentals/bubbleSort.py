52a09b97ffd31ccaa2ab202521454e34264fec6d
Mereilim Aitassova <mereilim@C02YK2U2JGH5.local>
2020-09-28 16:04:41 -0700

    Bubble sort

---
+++ _python/Python_Fundamentals/bubbleSort.py
@@ -0,0 +1,13 @@
+
+arr = [8,1,5,3,2,0]
+
+def bubbleSort(arr):
+    for j in range(len(arr)-1):
+        for i in range(len(arr)-1-j):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+    print(arr)
+    return arr
+
+bubbleSort(arr)
