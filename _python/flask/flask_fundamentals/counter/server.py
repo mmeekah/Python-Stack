c9a987b0d933fe99fa92da187e8ce00a8ac94067
Mika <ameekah93@gmail.com>
2020-10-02 12:07:16 -0400

    Counter Assgnmnt

---
+++ _python/flask/flask_fundamentals/counter/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, session
+app=Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route('/')
+def index():
+    if 'count' not in session:
+        session['count'] = 0
+    session['count'] += 1
+    return render_template('index.html', count=session['count'])
+
+
+@app.route('/addtwo')
+def addtwo():
+    session['count'] += 1
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    session['count'] = 0
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug =True)
+
