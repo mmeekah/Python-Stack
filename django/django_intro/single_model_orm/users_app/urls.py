7520dfe0ebcca5307230cc80592c77b37ef2faae
Mika <ameekah93@gmail.com>
2020-10-08 14:46:18 -0400

    Users with Templates(ORM)

--- django/django_intro/single_model_orm/users_app/urls.py
+++ django/django_intro/single_model_orm/users_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path('', views.index)
+    path('', views.index),
+    path('users', views.add_user)
 ]
--- django/django_intro/single_model_orm/users_app/views.py
+++ django/django_intro/single_model_orm/users_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Users
 
 # Create your views here.
@@ -7,3 +7,18 @@ def index(request):
         "all_the_users": Users.objects.all()
     }
     return render(request, "index.html", context)
+
+
+def add_user(request):
+    print(request.POST)
+# add_user function first create a new user in the db
+# HTML will show the user to the webpage
+    Users.objects.create( 
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        age=request.POST['age'],
+    )
+#first first_name is from models.py
+#second first_name is from index.html name field
+    return redirect('/')
