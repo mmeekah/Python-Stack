7d14f6956509cc60e99221fc2ee9075de1f89029
Mika <ameekah93@gmail.com>
2020-10-08 18:50:54 -0400

    Dojo-Ninja-Template-ORM

--- django/django_intro/dojo_ninjas_proj/dojo_ninja_app/urls.py
+++ django/django_intro/dojo_ninjas_proj/dojo_ninja_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-urlpatterns = [
 
+urlpatterns = [
     path('', views.index),
-    
+    path('dojo/create', views.add_dojo),
+    path('ninja/create', views.add_ninja),
+
 ]
--- django/django_intro/dojo_ninjas_proj/dojo_ninja_app/views.py
+++ django/django_intro/dojo_ninjas_proj/dojo_ninja_app/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Dojo, Ninja
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context={
+        'all_ninjas': Ninja.objects.all(),
+        'all_dojos': Dojo.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+
+
+def add_dojo(request):
+    print(request.POST)
+    
+    new_dojo =Dojo.objects.create( 
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        state=request.POST['state'],
+        desc=request.POST['desc'],
+    )
+    return redirect('/')
+
+
+def add_ninja(request):
+    print(request.POST)
+    # How do I get the supplier object with just the id??
+    dojos_id = request.POST['dojo']
+    user_instance = Dojo.objects.get(id=dojos_id)
+    # add item to DB
+    Ninja.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        dojo=user_instance
+    )
+    return redirect('/')
