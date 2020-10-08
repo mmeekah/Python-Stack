b92ffe61a1e0025c2a971da5b29cf23de37ef14b
Mika <ameekah93@gmail.com>
2020-10-08 14:20:11 -0400

    Users Shell

---
+++ django/django_intro/single_model_orm/users_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Users
+
+# Create your views here.
+def index(request):
+    context = {
+        "all_the_users": Users.objects.all()
+    }
+    return render(request, "index.html", context)
