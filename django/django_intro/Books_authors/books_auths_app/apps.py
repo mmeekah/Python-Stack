4723070447c973dc22d453acf056dc0078381023
Mika <ameekah93@gmail.com>
2020-10-08 21:54:14 -0400

    Books/Authors-Shell

---
+++ django/django_intro/Books_authors/books_auths_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BooksAuthsAppConfig(AppConfig):
+    name = 'books_auths_app'
---
+++ django/django_intro/Books_authors/books_auths_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255) 
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f'< Book ID {self.id}: {self.title} >'
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    notes= models.TextField()
+    books = models.ManyToManyField(Book, related_name="authors")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f'< Author ID {self.id}: {self.first_name} >'
+
+    
