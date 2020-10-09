59b6f8fdca71cfd912198615069fe5c1f5fe53ea
Mika <ameekah93@gmail.com>
2020-10-09 11:36:11 -0400

    Books/Auths-Template-ORM

--- django/django_intro/Books_authors/books_auths_app/urls.py
+++ django/django_intro/Books_authors/books_auths_app/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('book/create', views.add_book),
+    path('author/create', views.add_author),
+    path('book_to_author', views.add_book_to_author),
+    path('book_dets/<book_id>', views.display_book_dets),
+    path('author_dets/<auth_id>', views.display_author_dets),
+    path('author_to_book', views.add_author_to_book)
+
 
 ]
--- django/django_intro/Books_authors/books_auths_app/views.py
+++ django/django_intro/Books_authors/books_auths_app/views.py
@@ -1,5 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book, Author
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context={
+        'all_books': Book.objects.all(),
+        'all_authors': Author.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+
+def add_book(request):
+    print(request.POST)
+
+    new_book =  Book.objects.create(
+        title=request.POST['title'],
+        desc =request.POST['desc']
+    )
+    return redirect('/')
+
+def add_author(request):
+    print(request.POST)
+
+    
+    Author.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        notes=request.POST['notes'],
+        
+        
+    )
+    
+    return redirect('/')
+
+def display_author_dets(request, auth_id):
+    to_exclude = [book.id for book in Author.objects.get(id=auth_id).books.all()]
+    context = {
+        "author" : Author.objects.get(id=auth_id),
+        "author_books" : Author.objects.get(id=auth_id).books.all(),
+        "all_books" : Book.objects.exclude(id__in=to_exclude),
+    }
+    return render(request, "author_view.html", context)
+
+def add_author_to_book(request):
+    if request.method == "POST":
+        book_id = request.POST["b_id"]
+        author_id = request.POST["a_id"]
+        Book.objects.get(id=book_id).authors.add(Author.objects.get(id=author_id))
+    return redirect("/book_dets/"+book_id)
+
+    
+def add_book_to_author(request):
+    if request.method == "POST":
+        book_id = request.POST["b_id"]
+        author_id = request.POST["a_id"]
+        Author.objects.get(id=author_id).books.add(Book.objects.get(id=book_id))
+    return redirect("/author_dets/"+author_id)   
+
+def display_book_dets(request,book_id):
+    to_exclude = [author.id for author in Book.objects.get(id=book_id).authors.all()]
+    context = {
+        "book" : Book.objects.get(id=book_id),
+        "book_authors" : Book.objects.get(id=book_id).authors.all(),
+        "all_authors" : Author.objects.exclude(id__in=to_exclude),
+    }
+    return render(request, "book_view.html", context)
